37e660ca949247d605df7b780626f3d327e077ab
Nitiwat Thongkao <nitiwat007@gmail.com>
2017-08-08 10:41:31 +0700

    add new Authen Class and UnitTest testAuthen
    add new Authen Class and UnitTest testAuthen

---
+++ tests/testAuthen.php
@@ -0,0 +1,28 @@
+<?php
+  use PHPUnit\Framework\TestCase;
+
+  class testAuthen extends TestCase{
+
+    // public $authen;
+
+    public function test_GetUsername(){
+      $authen=new Authen("nitiwat.t","123456789");
+      $this->assertEquals("nitiwat.t",$authen->getUsername());
+    }
+
+    public function test_GetPassword(){
+      $authen=new Authen("nitiwat.t","123456789");
+      $this->assertEquals("123456789",$authen->getPassword());
+    }
+
+    public function test_GetUsernameFail(){
+      $authen=new Authen("nitiwat.t2","123456789");
+      $this->assertNotEquals("nitiwat.t",$authen->getUsername());
+    }
+
+    public function test_GetPasswordFail(){
+      $authen=new Authen("nitiwat.t","1234567890");
+      $this->assertNotEquals("123456789",$authen->getPassword());
+    }
+  }
+ ?>
