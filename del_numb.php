aa2ed172acec25a1f859ecaed75c62139720a823
Nitiwat Thongkao <nitiwat007@gmail.com>
2017-08-17 14:19:18 +0700

    Method delete complete
    Just change input name Submit to submit.

--- del_numb.php
+++ del_numb.php
@@ -14,7 +14,7 @@ $Name_Member = trim($_SESSION['Name_Member']);
 
     <body>
 
-        <?	
+        <?
         include("A_connect.inc");
 
         $ID_NumControl		 		= $_GET["ID_NumControl"];
@@ -24,24 +24,24 @@ $Name_Member = trim($_SESSION['Name_Member']);
         <table width="502" border="0" cellpadding="0" cellspacing="0" style="border-right:1px solid black; border-left:1px solid black; border-top:1px solid black; border-bottom:1px solid black;">
             <!--DWLayoutTable-->
             <form  name="form" method="post" action="datanumber.php">
-                <tr> 
+                <tr>
                     <td width="1" height="25">&nbsp;</td>
                     <td colspan="3" valign="top"><div align="center"><font size="2" face="MS Sans Serif, Tahoma, sans-serif">คุณแน่ใจว่าต้องการลบข้อมูลดังกล่าว</font></div></td>
                     <td width="13">&nbsp;</td>
                 </tr>
-                <tr> 
+                <tr>
                     <td height="62">&nbsp;</td>
-                    <td valign="bottom"><div align="right"> 
+                    <td valign="bottom"><div align="right">
                             <input  name="ID_NumControl" type="hidden" value="<? echo $ID_NumControl;?>">
                             <input type="hidden" name="action" value="del" />
-                            <input type="submit" name="Submit" value="ตกลง">
+                            <input type="submit" name="submit" value="ตกลง">
                         </div></td>
                     <td valign="top"><!--DWLayoutEmptyCell-->&nbsp;</td>
                     <td valign="bottom"><input type="reset" name="Submit2" value="ยกเลิก"></td>
                     <td>&nbsp;</td>
                 </tr>
             </form>
-            <tr> 
+            <tr>
                 <td height="0"></td>
                 <td></td>
                 <td></td>
